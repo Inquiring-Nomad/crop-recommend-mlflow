4ba744b1f68adc1bb351930288d742651c82daa7
Akis Loumpourdis <akis@akiss-air.home>
2021-06-15 21:31:46 +0100

    Added Heroku necessary files

--- production-app/app.py
+++ production-app/app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import  pickle
+import pickle
 import numpy as np
 
 
@@ -18,17 +18,15 @@ def predict(*args):
 st.title('What is the best crop to cultivate?')
 
 with st.form("crops"):
-
-    N = st.slider("N ratio", help="Ratio of Nitrogen content in soil", min_value=0,max_value=100)
-    P = st.slider("P ratio", help="Ratio of Phosphorous content in soil", min_value=0,max_value=100)
-    K = st.slider("K ratio", help="Ratio of Potassium content in soil", min_value=0,max_value=100)
-    humidity = st.slider("Humidity %", help="Relative humidity in %", min_value=0.0,max_value=100.0)
-    ph = st.slider("ph", help="ph value of the soil", min_value=0.0,max_value=14.0)
+    N = st.slider("N ratio", help="Ratio of Nitrogen content in soil", min_value=0, max_value=100)
+    P = st.slider("P ratio", help="Ratio of Phosphorous content in soil", min_value=0, max_value=100)
+    K = st.slider("K ratio", help="Ratio of Potassium content in soil", min_value=0, max_value=100)
+    humidity = st.slider("Humidity %", help="Relative humidity in %", min_value=0.0, max_value=100.0)
+    ph = st.slider("ph", help="ph value of the soil", min_value=0.0, max_value=14.0)
     temperature = st.number_input("Temperature (Celsius)", help="Temperature in degree Celsius", min_value=0.0)
     rainfall = st.number_input("Rainfall mm", help="rainfall in mm", min_value=0.0)
 
-
     submitted = st.form_submit_button("Submit")
     if submitted:
-        predictions = predict(N,P,K,temperature,humidity,ph,rainfall)
+        predictions = predict(N, P, K, temperature, humidity, ph, rainfall)
         st.success(f"Suggested crop to cultivate: {predictions.capitalize()}")
